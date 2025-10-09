d3c0792303435388d3b75af51160019edd6cbd5e
Dima <dimagr3214@gmail.com>
2025-10-09 10:42:13 +0300

    fix: linting and formating

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,33 +7,33 @@ import importPlugin from 'eslint-plugin-import';
 import prettier from 'eslint-config-prettier';
 
 export default [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        ignores: ['dist', 'node_modules'],
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}}'],
-        languageOptions: {
-            globals: globals.browser,
-            parser: tseslint.parser,
-            parserOptions: {
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-                ecmaFeatures: { jsx: true },
-            },
-        },
-        plugins: {
-            react,
-            'react-hooks': reactHooks,
-            import: importPlugin,
-        },
-        settings: {
-            react: { version: 'detect' },
-        },
-        rules: {
-            'react/react-in-jsx-scope': 'off',
-            '@typescript-eslint/no-unused-vars': ['warn'],
-            'import/order': ['warn', { alphabetize: { order: 'asc' } }],
-            ...prettier.rules,
-        },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist', 'node_modules'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}}'],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
     },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'import/order': ['warn', { alphabetize: { order: 'asc' } }],
+      ...prettier.rules,
+    },
+  },
 ];
