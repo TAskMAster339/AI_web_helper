c231606c914cb716e37e9d05861c97c7accc55b8
Dima <dimagr3214@gmail.com>
2025-10-12 22:58:15 +0300

    feat: add type checking pipeline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,41 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import Login from './components/login';
+import Register from './components/register';
+import NotFound from './components/notFound';
+import './style.css';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul className="flex mb-10">
+            <li className="mr-5">
+              <Link to="/">Главная</Link>
+            </li>
+            <li className="mr-5">
+              <Link to="/about">О нас</Link>
+            </li>
+            <li className="mr-5">
+              <Link to="/login">Логин</Link>
+            </li>
+            <li className="mr-5">
+              <Link to="/register">Регистрация</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
